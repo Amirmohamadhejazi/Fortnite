d907a30eaf493640cb2af5f2f311978046fefc3f
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2023-11-25 09:00:36 +0330

    fix: add scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,26 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
     important: true,
     content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-    theme: {},
+    theme: {
+        screens: {
+            xs: '500px',
+            // => @media (min-width: 500px) { ... }
+            sm: '640px',
+            // => @media (min-width: 640px) { ... }
+
+            md: '768px',
+            // => @media (min-width: 768px) { ... }
+
+            lg: '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            xl: '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+    },
     plugins: [],
     corePlugins: {},
 }
